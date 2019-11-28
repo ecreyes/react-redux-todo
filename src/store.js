16aca6df15e6773ea6fa99e48eaf5f3ebe9d3299
ecreyes <eduardo.reyeso.14@sansano.usm.cl>
2019-11-28 14:08:17 -0300

    redux devTools en el store

--- src/store.js
+++ src/store.js
@@ -1,5 +1,8 @@
 import { createStore } from 'redux'
+import { devToolsEnhancer } from 'redux-devtools-extension';
 import todoApp from './reducers/index';
 
-const store = createStore(todoApp);
+const store = createStore(todoApp,devToolsEnhancer(
+    // Specify name here, actionsBlacklist, actionsCreators and other options if needed
+  ));
 export default store;
